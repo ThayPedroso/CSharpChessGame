d56454c3536d15671eaee838a4e41d33c2f489f9
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-01-18 12:56:18 -0300

    Projeto criado

---
+++ CSharpChessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
