3cae1f5bc09a31b49c7077aafd85dedc60d4926b
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-01-19 16:33:49 -0300

    Criada a classe Posicao

--- CSharpChessGame/Program.cs
+++ CSharpChessGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace CSharpChessGame
 {
@@ -6,7 +7,13 @@ namespace CSharpChessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+
         }
     }
 }
