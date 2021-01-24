157f3b1899c2bb10ec207a36fe4407c2667e4b18
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-01-24 14:39:50 -0300

    Jogada especial promocao

--- CSharpChessGame/xadrez/PartidaDeXadrez.cs
+++ CSharpChessGame/xadrez/PartidaDeXadrez.cs
@@ -163,6 +163,20 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque! Cuidado.");
             }
 
+            Peca p = tab.peca(destino);
+            // promocao
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(p.cor, tab);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -181,8 +195,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // En Passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
